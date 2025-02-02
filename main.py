6cb4ff91b923f6f24dfcf6f3f7d81b281840003f
Ayush Gupta <59391849+Dark-Coders995@users.noreply.github.com>
2025-02-02 23:41:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,34 @@
-# main.py
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from typing import List
+import json
+from pydantic import BaseModel
+
+
+with open("q-vercel-python.json", "r") as file:
+    students = json.load(file)
+
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"message": "Hello, World!"}
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_credentials=True,
+    allow_methods=["GET"],  
+    allow_headers=["*"],  
+)
+
+
+def get_marks(names: List[str]) -> List[int]:
+    marks = []
+    student_dict = {student['name']: student['marks'] for student in students}
+    for name in names:
+        marks.append(student_dict.get(name, "Student not found"))
+    return marks
+
+@app.get("/api")
+async def get_student_marks(name: List[str]):
+    marks = get_marks(name)
+    return {"marks": marks}
